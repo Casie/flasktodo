5a9517347c4a21d273af1b52336ee050de157d6f
casie <casie.levine@gmail.com>
2015-06-10 22:57:39 -0400

    finished base todo - no formatting yet

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask, redirect, url_for
+import os
+
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient(
+	os.environ['TODO_DB_1_PORT_27017_TCP_ADDR'], 
+	27017)
+db = client.tododb
+
+
 @app.route('/')
 def todo():
-	return "Hello!"
+	_items = db.tododb.find()
+	items = [item for item in _items]
+
+	return render_template('todo.html', items=items)
 
 @app.route('/new', methods=['POST'])
 def new():
+
+	item_doc = {
+		'occasion': request.form['occasion'],
+		'invited_count': request.form['invited_count'],
+		'year': request.form['year'],
+		'month': request.form['month'],
+		'day': request.form['day'],
+	}
+	db.tododb.insert_one(item_doc)
+
 	return redirect(url_for('todo'))	
 
 if __name__ == "__main__":
